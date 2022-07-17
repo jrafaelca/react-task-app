6724d621ee86a48cd0bcb2947f1e45912a14ece1
Jose Carrizales <jrafaelca@gmail.com>
2022-07-17 15:06:45 -0400

    update app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import Layout from "./components/layouts/Default";
+import Card from "./components/base/Card";
+import Button from "./components/base/Button";
 
 function App() {
     return (
-        <h1 >
-            App
-        </h1>
+        <Layout>
+            <div className="flex flex-col items-center">
+                <div className="w-1/2">
+                    <Card title="Card title" description="Card description">
+                        <div className="flex justify-end space-x-2">
+                            <Button type="primary">Update</Button>
+                            <Button type="danger">Delete</Button>
+                        </div>
+                    </Card>
+                </div>
+            </div>
+        </Layout>
     );
 }
 
