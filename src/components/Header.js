e325061f9353a1f002472809e46b2c7922068695
Jose Carrizales <jrafaelca@gmail.com>
2022-07-17 18:02:26 -0400

    add links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom";
 import Button from "./base/Button";
 
 function Header() {
@@ -5,13 +6,15 @@ function Header() {
         <header className="py-6 text-gray-100 bg-gray-900">
             <div className="container mx-auto">
                 <div className="flex justify-between items-center">
-                    <a href="/" className="font-semibold">
-                        Task<span className="font-light">App</span>
-                    </a>
+                    <Link to="/">
+                        <span className="font-bold">Task</span>App
+                    </Link>
 
-                    <Button type="primary">
-                        Add task
-                    </Button>
+                    <Link to="/create">
+                        <Button type="primary">
+                            Create task
+                        </Button>
+                    </Link>
                 </div>
             </div>
         </header>
