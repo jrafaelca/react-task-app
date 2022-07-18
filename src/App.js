3822e5e800445dc8bc3132bf23a4a77084b4fe7e
Jose Carrizales <jrafaelca@gmail.com>
2022-07-17 23:44:46 -0400

    add functionality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import {Link} from "react-router-dom";
+import {useData} from "./context";
 import Layout from "./components/layouts/Default";
 import Card from "./components/base/Card";
 import Button from "./components/base/Button";
 
 function App() {
+    const {tasks, updateTask} = useData();
+
     return (
         <Layout>
-            <div className="flex flex-col items-center">
-                <div className="w-1/2">
-                    <Card title="Card title" description="Card description">
-                        <div className="flex justify-between space-x-2">
-                            <Link to="/edit">
-                                <Button type="success">Completed</Button>
-                            </Link>
-
-                            <div>
-                                <Link to="/edit">
-                                    <Button>Edit</Button>
-                                </Link>
-                                <Link to="/delete">
-                                    <Button>Delete</Button>
-                                </Link>
-                            </div>
+            <div className="flex flex-col items-center space-y-4">
+                {tasks.length === 0 &&
+                    <div className="w-1/2">
+                        <div className="text-2xl text-center text-gray-400 font">
+                            No tasks found
                         </div>
-                    </Card>
-                </div>
+                    </div>
+                }
+
+                {tasks.map((task, index) => (
+                    <div className="w-1/2" key={index}>
+                        <Card title={task.name} description={task.description} type={task.done}>
+                            <div className="flex justify-between ">
+                                <div className="space-x-3">
+                                    <Button type={task.done && 'success'}
+                                            onClick={() => updateTask({...task, done: !task.done})}>
+                                        {task.done ? 'Done' : 'Undone'}
+                                    </Button>
+                                </div>
+
+                                <div className="space-x-3">
+                                    <Link to={`/edit/${task.id}`}>
+                                        <Button>Edit</Button>
+                                    </Link>
+                                    <Link to={`/delete/${task.id}`}>
+                                        <Button>Delete</Button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
+                ))}
             </div>
         </Layout>
     );
--- src/Delete.js
+++ src/Delete.js
@@ -1,19 +1,47 @@
-import {Link} from "react-router-dom";
+import {useState, useEffect} from "react";
+import {Link, useNavigate, useParams} from "react-router-dom";
+
+// Context
+import {useData} from "./context";
+
+// Components
 import Layout from "./components/layouts/Default";
 import Card from "./components/base/Card";
 import Button from "./components/base/Button";
 
 function Delete() {
+    const navigate = useNavigate();
+    const params = useParams();
+    const {findTask, removeTask} = useData();
+
+    const [task, setTask] = useState({
+        id: null,
+        name: "",
+    })
+
+    useEffect(() => {
+        const task = findTask(params.id);
+        setTask(task);
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        removeTask(task.id);
+        navigate("/");
+    }
+
     return (
         <Layout>
             <div className="flex flex-col items-center">
                 <div className="w-1/2">
-                    <Card title="Card title" description="Card description">
+                    <Card title={`Delete ${task.name}`} description="The task will be permanently deleted.">
                         <div className="flex justify-end space-x-2">
                             <Link to="/">
                                 <Button>Back</Button>
                             </Link>
-                            <Button type="danger">Destroy</Button>
+                            <Button type="danger" onClick={handleSubmit}>
+                                Delete
+                            </Button>
                         </div>
                     </Card>
                 </div>
